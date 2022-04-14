f0594c9225955d1f3213d5b204197ed3ad8ce844
Alwin Stockinger <alwin.stockinger@tum.de>
2022-04-14 20:48:49 +0200

    get my videos implementation in index canister

--- src/index_canister/src/lib.rs
+++ src/index_canister/src/lib.rs
@@ -1,5 +1,5 @@
 use ic_cdk::export::candid::{Principal};
-use ic_cdk_macros::{update};
+use ic_cdk_macros::{update, query};
 use ic_cdk::storage;
 
 use std::collections::HashSet;
@@ -9,10 +9,9 @@ type VideoStore = HashMap<Principal,HashSet<Principal>>;
 
 #[update]
 pub async fn post_video(video_id: Principal){
+    let caller = ic_cdk::api::caller();
     let store = storage::get_mut::<VideoStore>();
     
-    let caller = ic_cdk::api::caller();
-
     let caller_videos = match store.get_mut(&caller){
         Some(caller_videos) => caller_videos,
         None => {
@@ -21,4 +20,12 @@ pub async fn post_video(video_id: Principal){
         }
     };
     caller_videos.insert(video_id);
+}
+
+#[query]
+pub async fn get_my_videos() -> Option<&'static HashSet<Principal>>{
+    let caller = ic_cdk::api::caller();
+    let store = storage::get_mut::<VideoStore>();
+
+    return store.get(&caller);
 }
