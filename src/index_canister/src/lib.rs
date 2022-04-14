f6248e7512b2a9ff8cf0a9f2b376da315d233591
Alwin Stockinger <alwin.stockinger@tum.de>
2022-04-14 20:57:08 +0200

    post and pre upgrade methods for index canister

--- src/index_canister/src/lib.rs
+++ src/index_canister/src/lib.rs
@@ -1,5 +1,5 @@
 use ic_cdk::export::candid::{Principal};
-use ic_cdk_macros::{update, query};
+use ic_cdk_macros::{update, query, pre_upgrade, post_upgrade};
 use ic_cdk::storage;
 
 use std::collections::HashSet;
@@ -28,4 +28,17 @@ pub async fn get_my_videos() -> Option<&'static HashSet<Principal>>{
     let store = storage::get_mut::<VideoStore>();
 
     return store.get(&caller);
+}
+
+#[pre_upgrade]
+fn pre_upgrade() {
+    let store = storage::get::<VideoStore>();
+    storage::stable_save((store,)).unwrap();
+}
+
+#[post_upgrade]
+fn post_upgrade() {
+    let (store,): (VideoStore,) = storage::stable_restore().unwrap();
+
+    *storage::get_mut::<VideoStore>() = store;
 }
