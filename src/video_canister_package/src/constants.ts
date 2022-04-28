1269ed5dd40d3218a6e25d48ca72fff3f336c1a0
Moritz Schindelmann <moritz.schindelmann@tum.de>
2022-04-28 14:12:21 +0200

    create env variables for test mode in dev

--- src/video_canister_package/src/constants.ts
+++ src/video_canister_package/src/constants.ts
@@ -1,14 +1,17 @@
+require('dotenv').config();
+
 import { idlFactory as videoCanisterIdl } from './canisters/video_canister/videoCanister_idl.did';
 import { idlFactory as spawnCanisterIdl } from './canisters/spawn_canister/spawnCanister_idl.did';
 import { idlFactory as managementCanisterIdl } from './canisters/management_canister/managementCanister_idl.did';
 import { idlFactory as walletCanisterIdl } from './canisters/wallet_canister/walletCanister_idl.did';
 import { idlFactory as indexCanisterIdl } from './canisters/index_canister/indexCanister_idl.did';
 
-// TODO adjust for mainnet
-export const SPAWN_PRINCIPAL_ID = 'ryjl3-tyaaa-aaaaa-aaaba-cai';
-export const INDEX_PRINCIPAL_ID = 'rkp4c-7iaaa-aaaaa-aaaca-cai'
+const DEV_MODE = process.env.NODE_ENV === 'DEV';
 
 // TODO adjust for mainnet
+export const SPAWN_PRINCIPAL_ID = DEV_MODE ? process.env.SPAWN_PRINCIPAL_ID || '' : 'ryjl3-tyaaa-aaaaa-aaaba-cai';
+export const INDEX_PRINCIPAL_ID = DEV_MODE ? process.env.INDEX_PRINCIPAL_ID || '' : 'rkp4c-7iaaa-aaaaa-aaaca-cai';
+
 export const MANAGEMENT_PRINCIPAL_ID = 'aaaaa-aa';
 
 export const REQUIRED_CYCLES: bigint = BigInt(200_000_000_000);
