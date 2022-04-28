95c489a8ca8c45575de9c33ebc83927bfe047881
Moritz Schindelmann <moritz.schindelmann@tum.de>
2022-04-28 15:21:02 +0200

    refac: encapsulate main functionality in class

--- src/video_canister_package/src/__tests__/video_canister.test.ts
+++ src/video_canister_package/src/__tests__/video_canister.test.ts
@@ -1,7 +1,7 @@
 import { AnonymousIdentity } from '@dfinity/agent';
 import { Principal } from '@dfinity/principal';
 import { Secp256k1KeyIdentity } from '@dfinity/identity';
-import { getVideo, uploadVideo, changeOwner, getMyVideos } from '../index';
+import { ICVideoStorage } from '../index';
 import { VideoToStore } from '../interfaces';
 
 jest.setTimeout(30_000);
@@ -10,8 +10,10 @@ const videoCanisterVersion = 0n;
 
 const anonWalletPrincipal = 'rno2w-sqaaa-aaaaa-aaacq-cai';
 
+const storage = new ICVideoStorage();
+
 test('getVideo', async () => {
-  const video = await getVideo(new AnonymousIdentity(), Principal.fromText('renrk-eyaaa-aaaaa-aaada-cai'));
+  const video = await storage.getVideo(new AnonymousIdentity(), Principal.fromText('renrk-eyaaa-aaaaa-aaada-cai'));
 
   expect(video.name).toBe('test_name');
   expect(video.videoBuffer).toStrictEqual(Buffer.from([0xca, 0xff, 0xee]));
@@ -29,9 +31,9 @@ test('createVideo', async () => {
 
   const creationCycles: bigint = BigInt(200_000_000_000);
 
-  const principal: Principal = await uploadVideo(anon, anonWallet, video, creationCycles, false);
+  const principal: Principal = await storage.uploadVideo(anon, anonWallet, video, creationCycles, false);
 
-  const uploadedVideo = await getVideo(anon, principal);
+  const uploadedVideo = await storage.getVideo(anon, principal);
 
   expect(uploadedVideo.name).toBe(video.name);
   expect(uploadedVideo.description).toBe(video.description);
@@ -52,15 +54,15 @@ test('changeOwner', async () => {
 
   const creationCycles: bigint = BigInt(300_000_000_000);
 
-  const videoPrincipal: Principal = await uploadVideo(anon, anonWallet, video, creationCycles, false);
+  const videoPrincipal: Principal = await storage.uploadVideo(anon, anonWallet, video, creationCycles, false);
 
   const newOwnerIdentity = Secp256k1KeyIdentity.generate();
   const newOwner = newOwnerIdentity.getPrincipal();
   const newOwnerWallet = Secp256k1KeyIdentity.generate().getPrincipal(); //Just for testing, of course the newOwner doesn't have access to the Wallet and behind the principal is no actual wallet but that doesn't matter for the test
 
-  await changeOwner(anon, anonWallet, videoPrincipal, newOwner, newOwnerWallet);
+  await storage.changeOwner(anon, anonWallet, videoPrincipal, newOwner, newOwnerWallet);
 
-  const uploadedVideo = await getVideo(anon, videoPrincipal);
+  const uploadedVideo = await storage.getVideo(anon, videoPrincipal);
 
   expect(uploadedVideo.owner).toStrictEqual(newOwner);
 
@@ -81,9 +83,9 @@ test('indexVideo', async () => {
 
   const creationCycles: bigint = BigInt(200_000_000_000);
 
-  const videoPrincipal: Principal = await uploadVideo(anon, anonWallet, video, creationCycles, true);
+  const videoPrincipal: Principal = await storage.uploadVideo(anon, anonWallet, video, creationCycles, true);
 
-  const myVideos: Principal[] = await getMyVideos(anon);
+  const myVideos: Principal[] = await storage.getMyVideos(anon);
 
   expect(myVideos.length).toBe(1);
   expect(myVideos[0]).toStrictEqual(videoPrincipal);
--- src/video_canister_package/src/index.ts
+++ src/video_canister_package/src/index.ts
@@ -14,125 +14,127 @@ import {
 import { CANISTER_TYPE, CHUNK_SIZE, REQUIRED_CYCLES, INDEX_PRINCIPAL_ID } from './constants';
 import { VideoToStore, Video } from './interfaces';
 
-export async function uploadVideo(
-  identity: Identity,
-  walletId: Principal,
-  video: VideoToStore,
-  cycles: bigint,
-  save: boolean,
-): Promise<Principal> {
-  if (cycles < REQUIRED_CYCLES) {
-    throw Error('Not enough cycles, need at least ' + REQUIRED_CYCLES + ' for video canister creation');
-  }
+export class ICVideoStorage {
+  async uploadVideo(
+    identity: Identity,
+    walletId: Principal,
+    video: VideoToStore,
+    cycles: bigint,
+    save: boolean,
+  ): Promise<Principal> {
+    if (cycles < REQUIRED_CYCLES) {
+      throw Error('Not enough cycles, need at least ' + REQUIRED_CYCLES + ' for video canister creation');
+    }
 
-  const videoPrincipal = await createNewCanister(identity, walletId, REQUIRED_CYCLES);
+    const videoPrincipal = await createNewCanister(identity, walletId, REQUIRED_CYCLES);
 
-  await checkController(identity, walletId, videoPrincipal);
+    await checkController(identity, walletId, videoPrincipal);
 
-  const leftoverCycles = cycles - REQUIRED_CYCLES;
-  if (leftoverCycles > 0) {
-    await depositCycles(identity, walletId, videoPrincipal, leftoverCycles);
-  }
+    const leftoverCycles = cycles - REQUIRED_CYCLES;
+    if (leftoverCycles > 0) {
+      await depositCycles(identity, walletId, videoPrincipal, leftoverCycles);
+    }
 
-  const videoActor = await getCanisterActor(identity, CANISTER_TYPE.VIDEO_CANISTER, videoPrincipal);
+    const videoActor = await getCanisterActor(identity, CANISTER_TYPE.VIDEO_CANISTER, videoPrincipal);
 
-  let chunkNum = 0;
-  if (video.videoBuffer.length !== 0) {
-    chunkNum = Math.floor(video.videoBuffer.length / CHUNK_SIZE) + 1;
-  }
-
-  await executeVideoCanisterPut(
-    () =>
-      videoActor.put_meta_info({
-        name: video.name,
-        description: video.description,
-        chunk_num: chunkNum,
-      }),
-    'Could not put meta info into video canister',
-  );
-
-  for (let i = 0; i < chunkNum; i++) {
-    const chunkSlice = video.videoBuffer.slice(
-      i * CHUNK_SIZE,
-      Math.min(video.videoBuffer.length, (i + 1) * CHUNK_SIZE),
-    );
-    const chunkArray = Array.from(chunkSlice);
+    let chunkNum = 0;
+    if (video.videoBuffer.length !== 0) {
+      chunkNum = Math.floor(video.videoBuffer.length / CHUNK_SIZE) + 1;
+    }
 
     await executeVideoCanisterPut(
-      () => videoActor.put_chunk(i, chunkArray),
-      `Could not put chunk <${i}> into the video canister`,
+      () =>
+        videoActor.put_meta_info({
+          name: video.name,
+          description: video.description,
+          chunk_num: chunkNum,
+        }),
+      'Could not put meta info into video canister',
     );
+
+    for (let i = 0; i < chunkNum; i++) {
+      const chunkSlice = video.videoBuffer.slice(
+        i * CHUNK_SIZE,
+        Math.min(video.videoBuffer.length, (i + 1) * CHUNK_SIZE),
+      );
+      const chunkArray = Array.from(chunkSlice);
+
+      await executeVideoCanisterPut(
+        () => videoActor.put_chunk(i, chunkArray),
+        `Could not put chunk <${i}> into the video canister`,
+      );
+    }
+
+    if (save) {
+      const indexActor = await getCanisterActor(
+        identity,
+        CANISTER_TYPE.INDEX_CANISTER,
+        Principal.fromText(INDEX_PRINCIPAL_ID),
+      );
+      await indexActor.post_video(videoPrincipal);
+    }
+
+    return videoPrincipal;
   }
 
-  if (save) {
+  async getVideo(identity: Identity, principal: Principal): Promise<Video> {
+    const actor = await getCanisterActor(identity, CANISTER_TYPE.VIDEO_CANISTER, principal);
+
+    try {
+      const metaInfo = (await actor.get_meta_info()) as MetaInfo;
+      const chunksAsPromises = [];
+
+      for (let i = 0; i < metaInfo.chunk_num; i++) {
+        chunksAsPromises.push(actor.get_chunk(i) as Promise<Array<number[] | undefined[]>>);
+      }
+
+      const chunkBuffers: Uint8Array[] = [];
+
+      const nestedBytes = (await Promise.all(chunksAsPromises))
+        .map((val) => (val[0] ? val[0] : null))
+        .filter((v) => v !== null) as number[][];
+
+      nestedBytes.forEach((bytes) => {
+        const bytesAsBuffer = Buffer.from(new Uint8Array(bytes));
+        chunkBuffers.push(bytesAsBuffer);
+      });
+
+      return {
+        name: metaInfo.name,
+        description: metaInfo.description,
+        version: metaInfo.version,
+        owner: metaInfo.owner,
+        videoBuffer: Buffer.concat(chunkBuffers),
+      };
+    } catch (error) {
+      throw new Error('Unable to query video: ' + error);
+    }
+  }
+
+  async changeOwner(
+    oldIdentity: Identity,
+    oldWallet: Principal,
+    videoPrincipal: Principal,
+    newOwner: Principal,
+    newOwnerWallet: Principal,
+  ) {
+    await changeCanisterController(oldIdentity, oldWallet, videoPrincipal, newOwnerWallet);
+
+    await changeVideoOwner(oldIdentity, videoPrincipal, newOwner);
+  }
+
+  async getMyVideos(identity: Identity): Promise<Principal[]> {
     const indexActor = await getCanisterActor(
       identity,
       CANISTER_TYPE.INDEX_CANISTER,
       Principal.fromText(INDEX_PRINCIPAL_ID),
     );
-    await indexActor.post_video(videoPrincipal);
-  }
+    const optVideos = (await indexActor.get_my_videos()) as [[Principal]];
 
-  return videoPrincipal;
-}
-
-export async function getVideo(identity: Identity, principal: Principal): Promise<Video> {
-  const actor = await getCanisterActor(identity, CANISTER_TYPE.VIDEO_CANISTER, principal);
-
-  try {
-    const metaInfo = (await actor.get_meta_info()) as MetaInfo;
-    const chunksAsPromises = [];
-
-    for (let i = 0; i < metaInfo.chunk_num; i++) {
-      chunksAsPromises.push(actor.get_chunk(i) as Promise<Array<number[] | undefined[]>>);
+    if (optVideos[0] === undefined) {
+      return new Array<Principal>();
+    } else {
+      return optVideos[0];
     }
-
-    const chunkBuffers: Uint8Array[] = [];
-
-    const nestedBytes = (await Promise.all(chunksAsPromises))
-      .map((val) => (val[0] ? val[0] : null))
-      .filter((v) => v !== null) as number[][];
-
-    nestedBytes.forEach((bytes) => {
-      const bytesAsBuffer = Buffer.from(new Uint8Array(bytes));
-      chunkBuffers.push(bytesAsBuffer);
-    });
-
-    return {
-      name: metaInfo.name,
-      description: metaInfo.description,
-      version: metaInfo.version,
-      owner: metaInfo.owner,
-      videoBuffer: Buffer.concat(chunkBuffers),
-    };
-  } catch (error) {
-    throw new Error('Unable to query video: ' + error);
-  }
-}
-
-export async function changeOwner(
-  oldIdentity: Identity,
-  oldWallet: Principal,
-  videoPrincipal: Principal,
-  newOwner: Principal,
-  newOwnerWallet: Principal,
-) {
-  await changeCanisterController(oldIdentity, oldWallet, videoPrincipal, newOwnerWallet);
-
-  await changeVideoOwner(oldIdentity, videoPrincipal, newOwner);
-}
-
-export async function getMyVideos(identity: Identity): Promise<Principal[]> {
-  const indexActor = await getCanisterActor(
-    identity,
-    CANISTER_TYPE.INDEX_CANISTER,
-    Principal.fromText(INDEX_PRINCIPAL_ID),
-  );
-  const optVideos = (await indexActor.get_my_videos()) as [[Principal]];
-
-  if (optVideos[0] === undefined) {
-    return new Array<Principal>();
-  } else {
-    return optVideos[0];
   }
 }
